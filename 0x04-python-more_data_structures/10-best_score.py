f6d1c36c14d57ca146e3f489ff86ac83dcf30857
matthew <matthewkadewole@gmail.com>
2022-12-08 08:35:05 -0800

    correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,13 @@
-
-
-
-
 #!/usr/bin/python3
+
+
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None:
         return None
-    biggest = 0
-    for key, value in a_dictionary.items():
-        if value > biggest:
-            biggest = value
-    for key, value in a_dictionary.items():
-        if value == biggest:
-            return 	
+    maxval = 0
+    maxkey = None
+    for k, v in a_dictionary.items():
+        if v > maxval:
+            maxval = v
+            maxval = k
+    return maxkey
